296330f52291c26dc9ccd2d884dab22a07a91a3a
Michele Emiliani <michele.emiliani@kape.com>
2023-10-09 19:05:20 +0200

    PIA-689: Manage 1 testapp TCP flow
    When a managed application sends traffic on a new socket,
    handleNewFlow is called. (just the first time)
    
    A new socket, managed by the network extension process,
    is created pointing to the same endpoint and port of the original one.
    
    We start by reading the OUTBOUT traffic of the application
    using readTCPFlowData.
    We then write the same data to the endpoint via the newly created socket.
    (If the endpoint is a netcat on listen, you will see the data)
    
    In readTCPFlowData closure we first write any INBOUND traffic coming
    from the new socket back to the application.
    We then call readTCPFlowData and block waiting for new OUTBOUND traffic.
    
    Binding and support for multiple connections will be handled on new PRs.

--- ProxyExtension/HandlingFlows.swift
+++ ProxyExtension/HandlingFlows.swift
@@ -5,113 +5,108 @@ import os.log
 @available(macOS 11.0, *)
 extension STProxyProvider {
     
-    // This method is called whenever an app that
-    // matches the current App Proxy configuration’s app rules
-    // sends TCP or UDP network traffic.
-    // returns:
-    //   true  -> to handle a new flow
-    //   false -> (for NETransparentProxyProvider)
-    //            to let the system handle the flow
-    //   false -> (for NEAppProxyProvider or NEDNSProxyProvider)
-    //            the flow is discarded and the connection is closed
+    // handleNewFlow is called whenever an application
+    // creates a new TCP/UDP socket.
+    //
+    //   return true  -> to handle a new flow
+    //     The flow of this app will be managed by the network extension
+    //   return false -> (NETransparentProxyProvider)
+    //     The flow of this app will NOT be managed.
+    //     It will be routed using the system's routing tables
+    //   return false -> (NEAppProxyProvider or NEDNSProxyProvider)
+    //     The flow is discarded and the connection is closed
+    //
     override func handleNewFlow(_ flow: NEAppProxyFlow) -> Bool {
-        // the name of the application to which this flow belongs
         let appID = flow.metaData.sourceAppSigningIdentifier
-        
         os_log("deciding if we need to handle %s flow", appID)
-        
+                
         // A condition could be added here to achieve inverse split tunnelling.
         // Given the list of apps, we could either:
         // - manage the flows of ONLY the apps in the list
         // - manage the flows of ALL the OTHER apps, EXCEPT the ones in the list.
-        //   (If implemented, this would need to be tested performance-wise)
-        
+        //
         if appsToManage!.contains(appID) {
-            redirectFlow(flow)
-            
-            // The flow of this app will be managed by the network extension
-            // The objective is that this traffic goes through the physical
-            // network interface.
-            // As if all the sockets of this process were bound to that
-            // interface.
+            // flow.open() must be called before returning true in handleNewFlow
+            manageFlow(flow)
             return true
         }
-        
-        // The flow of this app will be routed by the system as if the
-        // transparent proxy was not there.
-        // The idea is to run the extension only when we are connected to the vpn,
-        // so that this traffic goes through the vpn virtual network interface.
         return false
     }
     
-    private func redirectFlow(_ flow: NEAppProxyFlow) -> Void {
-        // Check if the connection state is not "connected".
-        // Ideally we have already checked that during extension init.
-        // It is not bad to always check it here though, since the connection
-        // might have been closed due to the localProxy process crashing or exiting
-        if self.connection!.state != .connected {
-            os_log("Connection to local server not active!")
-            return
-        }
-        
-        // read-only info about the flow
-        // same as appID
-        let description = flow.metaData.debugDescription
-        // the PID of the process can be extracted from the sourceAppAuditToken
-        let appAuditToken = flow.metaData.sourceAppAuditToken
-        // the ip address destination of the connection, if present
-        let remoteHostname = flow.remoteHostname
-        // the network interface of the flow (en0)
-        let nwInterface = flow.networkInterface?.description
-        
-        // NEAppProxyTCPFlow: An object for reading and writing data
-        // to and from a TCP connection being proxied by the provider
+    private func manageFlow(_ flow: NEAppProxyFlow) {
         if let TCPFlow = flow as? NEAppProxyTCPFlow {
-            // adding this flow to the vector containing all flows handled
-            // by the transparent proxy provider
-            TCPFlowsToHandle.append(TCPFlow)
             manageTCPFlow(TCPFlow)
         }
         else if let UDPFlow = flow as? NEAppProxyUDPFlow {
-            UDPFlowsToHandle.append(UDPFlow)
             manageUDPFlow(UDPFlow)
         }
     }
     
-    private func manageTCPFlow(_ flow: NEAppProxyTCPFlow) -> Void {
-        // An NWHostEndpoint object that contains the address and port to
-        // set as the local address and local port of the flow.
-        // If the source application already specifed a local endpoint by
-        // binding the socket, then this parameter is ignored.
-        // TODO: Check why this variable is not used
-        let localEndpoint: NWHostEndpoint
-        
+    // MARK: Managing TCP flows
+    private func manageTCPFlow(_ TCPFlow: NEAppProxyTCPFlow) {
         // open() is used by an NEProvider implementation
         // to indicate to the system that the caller is ready
         // to start reading and writing to this flow.
-        flow.open(withLocalEndpoint: nil) { error in
+        //
+        // LocalEndpoint: The address and port that should be used as the
+        // local endpoint of the socket associated with this flow.
+        // If the source application already specifed a local endpoint
+        // by binding the socket then this parameter is ignored
+        TCPFlow.open(withLocalEndpoint: nil) { error in
+            // this is an escaping closure, therefore it is async
+            // and can outlive the manageTCPFlow function
             if (error != nil) {
                 os_log("error during flow open! %s", error.debugDescription)
             }
             
-            self.readTCPFlowData(flow)
+            // read-only info about the flow
+            let appName = TCPFlow.metaData.sourceAppSigningIdentifier
+            let remoteEndpoint = TCPFlow.remoteEndpoint.description
+            let parts = remoteEndpoint.split(separator: ":", maxSplits: 1)
+            guard parts.count == 2 else {
+                return
+            }
+            let remoteEndpointAddress = String(parts[0])
+            let remoteEndpointPort = UInt16(parts[1])!
+            
+            // Create the socket that will proxy the traffic
+            let socket = TCPSocket(host: remoteEndpointAddress, port: remoteEndpointPort)
+            socket.create()
+            socket.connectToHost()
+            
+            // We read from the flow:
+            // that means reading the OUTBOUND traffic of the application
+            self.readTCPFlowData(TCPFlow, socket)
         }
         
         // close the flow when you dont want to read and write to it anymore
+        // TODO: is that needed to restore "normal" traffic for the applications that we stop managing?
+        // Yes, probably when we disable split tunnelling.
+        // Does the app work properly, or does it have to be restarted?
         // TCPFlow.closeReadWithError(nil)
         // TCPFlow.closeWriteWithError(nil)
     }
     
-    // TODO: Check if this is the correct way of reading and writing to a connection.
-    private func readTCPFlowData(_ flow: NEAppProxyTCPFlow) -> Void {
-        flow.readData { data, error in
+    private func readTCPFlowData(_ TCPFlow: NEAppProxyTCPFlow, _ socket: TCPSocket) {
+        // If data has a length of 0 then no data can be subsequently read from the flow.
+        // The completion handler is only called for the single read operation that was
+        // initiated by calling this method.
+        //
+        // If the caller wants to read more data then it should call this method again
+        // to schedule another read operation and another execution of the
+        // completion handler block.
+        // This call is blocking: until some data is read the closure will not be called
+        TCPFlow.readData { data, error in
             if error == nil, let readData = data, !readData.isEmpty {
-                self.connection!.write(readData, completionHandler: { connectionError in
-                    if connectionError == nil {
-                        self.writeTCPFlowData(flow)
-                        self.readTCPFlowData(flow)
+                // writing to the real endpoint (via socket) what we read from the flow.
+                // This is the application OUTBOUND traffic
+                socket.writeData(readData, completionHandler: { writeError in
+                    if writeError == nil {
+                        // wait for answer from the endpoint
+                        self.writeTCPFlowData(TCPFlow, socket)
+                        self.readTCPFlowData(TCPFlow, socket)
                     } else {
-                        os_log("error during connection write! %s", connectionError.debugDescription)
+                        os_log("error during socket writeData! %s", writeError.debugDescription)
                     }
                 })
             } else {
@@ -119,18 +114,22 @@ extension STProxyProvider {
                 if error != nil {
                     os_log("error during flow read! %s", error.debugDescription)
                 } else {
-                    os_log("read no data during flow read!")
+                    // TCPFlow.readData returned 0 data, so no data can be read from the flow.
+                    // We try calling TCPFlow.readData anyway
+                    //self.readTCPFlowData(TCPFlow, socket)
                 }
             }
         }
     }
     
-    private func writeTCPFlowData(_ flow: NEAppProxyTCPFlow) -> Void {
-        self.connection!.readMinimumLength(1, maximumLength: 2048, completionHandler: { data, error in
+    private func writeTCPFlowData(_ TCPFlow: NEAppProxyTCPFlow, _ socket: TCPSocket) {
+        // This call is blocking: until some data is read the closure will not be called
+        socket.readData(completionHandler: { data, error in
             if error == nil, let writeData = data, !writeData.isEmpty {
-                flow.write(writeData) { flowError in
+                TCPFlow.write(writeData) { flowError in
                     if flowError == nil {
-                        self.writeTCPFlowData(flow)
+                        //self.readTCPFlowData(TCPFlow, socket)
+                        os_log("no op")
                     } else {
                         os_log("error during flow write! %s", flowError.debugDescription)
                     }
@@ -140,15 +139,16 @@ extension STProxyProvider {
                 if error != nil {
                     os_log("error during connection read! %s", error.debugDescription)
                 } else {
-                    os_log("read no data during connection read!")
+                    os_log("read no data from socket read!")
                 }
             }
         })
     }
     
+    // MARK: Managing UDP flows
     // TODO: This function is missing implementation
-    private func manageUDPFlow(_ flow: NEAppProxyUDPFlow) -> Void {
-        flow.open(withLocalEndpoint: flow.localEndpoint as? NWHostEndpoint) { error in
+    private func manageUDPFlow(_ UDPFlow: NEAppProxyUDPFlow) {
+        UDPFlow.open(withLocalEndpoint: UDPFlow.localEndpoint as? NWHostEndpoint) { error in
             if let error = error {
                 os_log("Failed to open UDP flow: \(error)")
                 // close flow here
