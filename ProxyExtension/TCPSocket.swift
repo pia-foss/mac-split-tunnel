d96da47beaca0447f2c42c4f0622ba1c27a974a1
Michele Emiliani <michele.emiliani@kape.com>
2023-10-20 21:41:12 +0200

    PIA-731: Add implementation for getNetworkInterfaceIP function

--- ProxyExtension/TCPSocket.swift
+++ ProxyExtension/TCPSocket.swift
@@ -1,6 +1,7 @@
 import Foundation
 import Darwin
 import os.log
+import Network
 
 enum SocketStatus {
     case empty
@@ -107,10 +108,29 @@ class TCPSocket {
             status = .closed
         }
     }
-    
-    // TODO: Implement this method
-    private func getNetworkInterfaceIP(interfaceName: String) -> String {
-        return "192.168.2.220"
+
+    func getNetworkInterfaceIP(interfaceName: String) -> String? {
+        var address: String?
+        var ifaddr: UnsafeMutablePointer<ifaddrs>? = nil
+        if getifaddrs(&ifaddr) == 0 {
+            var ptr = ifaddr
+            while ptr != nil {
+                defer { ptr = ptr?.pointee.ifa_next }
+
+                guard let interface = ptr?.pointee else { return nil }
+                let addrFamily = interface.ifa_addr.pointee.sa_family
+                if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+                    let name: String = String(cString: (interface.ifa_name))
+                    if  name == interfaceName {
+                        var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                        getnameinfo(interface.ifa_addr, socklen_t((interface.ifa_addr.pointee.sa_len)), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST)
+                        address = String(cString: hostname)
+                    }
+                }
+            }
+            freeifaddrs(ifaddr)
+        }
+        return address
     }
     
     func bindToNetworkInterface(interfaceName: String) -> Bool {
