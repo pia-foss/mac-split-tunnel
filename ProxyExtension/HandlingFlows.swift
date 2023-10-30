ad6f5fe6a2f0665c1db226b37eaaf4b384634e3d
Michele Emiliani <michele.emiliani@kape.com>
2023-10-30 15:04:45 +0100

    PIA-756: Move TCP specific functions to a new file

--- ProxyExtension/HandlingFlows.swift
+++ ProxyExtension/HandlingFlows.swift
@@ -5,6 +5,7 @@ import os.log
 @available(macOS 11.0, *)
 extension STProxyProvider {
     
+    // MARK: Managing TCP flows
     // handleNewFlow() is called whenever an application
     // creates a new TCP socket.
     //
@@ -33,23 +34,6 @@ extension STProxyProvider {
         return false
     }
     
-    // handleNewUDPFlow() is called whenever an application
-    // creates a new UDP socket.
-    //
-    // By overriding this method, all UDP flows will be
-    // caught by this function instead of handleNewFlow()
-    override func handleNewUDPFlow(_ flow: NEAppProxyUDPFlow, initialRemoteEndpoint remoteEndpoint: NWEndpoint) -> Bool {
-        let appID = flow.metaData.sourceAppSigningIdentifier
-        
-        if appsToManage!.contains(appID) {
-            os_log("managing %s UDP flow", appID)
-            manageUDPFlow(flow, remoteEndpoint)
-            return true
-        }
-        return false
-    }
-    
-    // MARK: Managing TCP flows
     private func manageTCPFlow(_ tcpFlow: NEAppProxyTCPFlow) {
         // open() is used by an NEProvider implementation
         // to indicate to the system that the caller is ready
@@ -87,84 +71,23 @@ extension STProxyProvider {
         }
     }
     
-    private func closeFlow(_ tcpFlow: NEAppProxyTCPFlow) {
-        // close the flow when you dont want to read and write to it anymore
-        tcpFlow.closeReadWithError(nil)
-        tcpFlow.closeWriteWithError(nil)
-    }
-    
-    private func readTCPFlowData(_ tcpFlow: NEAppProxyTCPFlow, _ socket: Socket) {
-        // If data has a length of 0 then no data can be subsequently read from the flow.
-        // The completion handler is only called for the single read operation that was
-        // initiated by calling this method.
-        //
-        // If the caller wants to read more data then it should call this method again
-        // to schedule another read operation and another execution of the
-        // completion handler block.
-        // Reading the application OUTBOUND traffic
-        // This call is blocking: until some data is read the closure will not be called
-        tcpFlow.readData { dataReadFromFlow, flowError in
-            if flowError == nil, let dataToWriteToSocket = dataReadFromFlow, !dataToWriteToSocket.isEmpty {
-                // Writing to the real endpoint (via the local socket) the application OUTBOUND traffic
-                if socket.status == .closed {
-                    self.closeFlow(tcpFlow)
-                    return
-                }
-                socket.writeData(dataToWriteToSocket, completionHandler: { socketError in
-                    if socketError == nil {
-                        // wait for answer from the endpoint
-                        self.writeTCPFlowData(tcpFlow, socket)
-                        self.readTCPFlowData(tcpFlow, socket)
-                    } else { // handling errors for socket send()
-                        os_log("error during socket writeData! %s", socketError.debugDescription)
-                        socket.closeConnection()
-                        self.closeFlow(tcpFlow)
-                    }
-                })
-            } else { // handling errors for flow readData()
-                if flowError != nil {
-                    os_log("error during flow read! %s", flowError.debugDescription)
-                } else { // is reading 0 data from a flow different than getting an error? (verify this!)
-                    os_log("read no data from flow readData()")
-                }
-                // no op: We stop calling readTCPFlowData(), ending the recursive loop
-                socket.closeConnection()
-                self.closeFlow(tcpFlow)
-            }
-        }
-    }
-    
-    private func writeTCPFlowData(_ tcpFlow: NEAppProxyTCPFlow, _ socket: Socket) {
-        if socket.status == .closed {
-            self.closeFlow(tcpFlow)
-            return
-        }
-        // This call is blocking: until some data is read the closure will not be called
-        socket.readData(completionHandler: { dataReadFromSocket, socketError in
-            if socketError == nil, let dataToWriteToFlow = dataReadFromSocket, !dataToWriteToFlow.isEmpty {
-                tcpFlow.write(dataToWriteToFlow) { flowError in
-                    if flowError == nil {
-                        // no op, if write executed correctly
-                    } else {
-                        os_log("error during flow write! %s", flowError.debugDescription)
-                        socket.closeConnection()
-                        self.closeFlow(tcpFlow)
-                    }
-                }
-            } else { // handling socket readData() errors or read 0 data from socket
-                if socketError == nil {
-                    os_log("read no data from socket readData()") // is this error different from the other one? (verify this!)
-                } else {
-                    os_log("error during connection read! %s", socketError.debugDescription)
-                }
-                socket.closeConnection()
-                self.closeFlow(tcpFlow)
-            }
-        })
-    }
-    
     // MARK: Managing UDP flows
-    // TODO: This function is missing implementation
+    // handleNewUDPFlow() is called whenever an application
+    // creates a new UDP socket.
+    //
+    // By overriding this method, all UDP flows will be
+    // caught by this function instead of handleNewFlow()
+    override func handleNewUDPFlow(_ flow: NEAppProxyUDPFlow, initialRemoteEndpoint remoteEndpoint: NWEndpoint) -> Bool {
+        let appID = flow.metaData.sourceAppSigningIdentifier
+        
+        if appsToManage!.contains(appID) {
+            os_log("managing %s UDP flow", appID)
+            manageUDPFlow(flow, remoteEndpoint)
+            return true
+        }
+        return false
+    }
+    
     private func manageUDPFlow(_ udpFlow: NEAppProxyUDPFlow, _ endpoint: NWEndpoint) {
         udpFlow.open(withLocalEndpoint: udpFlow.localEndpoint as? NWHostEndpoint) { error in
             if let error = error {
@@ -174,5 +97,11 @@ extension STProxyProvider {
             }
         // continue by calling open() on udpFlow
         }
+    }
+    
+    func closeFlow(_ flow: NEAppProxyFlow) {
+        // close the flow when you dont want to read and write to it anymore
+        flow.closeReadWithError(nil)
+        flow.closeWriteWithError(nil)
     }
 }
