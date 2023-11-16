ac73f63a4b9afdc9b8cb3c11470093d67d849cf7
Michele Emiliani <michele.emiliani@kape.com>
2023-11-16 17:56:06 +0100

    PIA-918: Move error related function to ErrorHandling file

---
+++ ProxyExtension/ErrorHandling.swift
@@ -0,0 +1,24 @@
+import Foundation
+import NetworkExtension
+import os.log
+
+@available(macOS 11.0, *)
+func handleError(_ error: Error?, _ operation: String, _ flow: NEAppProxyFlow, _ socket: Socket) {
+    // We close both the flow and the connection when:
+    // - Any I/O operation return an error
+    // - We read or write 0 data to a flow
+    // - We read or write 0 data to a socket
+    if error != nil {
+        os_log("error during %s: %s", operation, error.debugDescription)
+    } else {
+        os_log("read no data from %s", operation)
+    }
+    socket.close()
+    closeFlow(flow)
+}
+
+func closeFlow(_ flow: NEAppProxyFlow) {
+    // close the flow when you dont want to read and write to it anymore
+    flow.closeReadWithError(nil)
+    flow.closeWriteWithError(nil)
+}
--- ProxyExtension/TCPFlowsIO.swift
+++ ProxyExtension/TCPFlowsIO.swift
@@ -61,18 +61,4 @@ class TCPIO {
             }
         }
     }
-
-    private static func handleError(_ error: Error?, _ operation: String, _ flow: NEAppProxyTCPFlow, _ socket: Socket) {
-        // We close both the flow and the connection when:
-        // - Any I/O operation return an error
-        // - We read or write 0 data to a flow
-        // - We read or write 0 data to a socket
-        if error != nil {
-            os_log("error during %s: %s", operation, error.debugDescription)
-        } else {
-            os_log("read no data from %s", operation)
-        }
-        socket.close()
-        closeFlow(flow)
-    }
 }
