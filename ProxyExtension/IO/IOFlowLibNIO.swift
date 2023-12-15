c2de396972632a44176174dfdc06507b1dad0fea
Michele Emiliani <michele.emiliani@kape.com>
2023-12-15 21:37:21 +0100

    PIA-1048: Handle TCP flow read for 1 flow using SwiftNIO
    function linkFlowAndChannel() is just a placeholder for a proper method to
    store multiple flows and channels.
    The objective is being able to get the corresponding channel from a flow
    and viceversa

--- ProxyExtension/IO/IOFlowLibNIO.swift
+++ ProxyExtension/IO/IOFlowLibNIO.swift
@@ -2,7 +2,30 @@ import Foundation
 import NetworkExtension
 import NIO
 
-var myFlow: NEAppProxyTCPFlow?
+// TODO: Change this to an hash map / list
+// instead of a global variable, to improve testability, this could be:
+// - a singleton class holding the list, we inject in IOFlowLibNIO and in the handlers
+// - an even better idea?
+var myFlow: NEAppProxyFlow?
+var myChannel: Channel?
+
+// TODO: Make this add a new pair to an hash map / list
+// so that it is always possible to get one from the other, possibly in O(1)
+func linkFlowAndChannel(_ flow: NEAppProxyFlow, _ channel: Channel) {
+    myFlow = flow
+    myChannel = channel
+}
+
+// this is not used atm
+func getFlow(channel: Channel) -> NEAppProxyFlow {
+    return myFlow!
+}
+
+func getChannel(flow: NEAppProxyFlow) -> Channel {
+    return myChannel!
+}
+
+
 
 final class IOFlowLibNIO : IOFlowLib {
     let eventLoopGroup: MultiThreadedEventLoopGroup
@@ -11,7 +34,7 @@ final class IOFlowLibNIO : IOFlowLib {
     init(interfaceName: String) {
         self.interfaceAddress = getNetworkInterfaceIP(interfaceName: interfaceName)!
         // trying with just 1 thread for now, since we dont want to use too many resources on the user's machines.
-        // SwiftNIO docs says it is still better to use MultiThreadedEventLoopGroup, even in the case of 1 thread used
+        // SwiftNIO docs says it is still better to use MultiThreadedEventLoopGroup, even in the case of just 1 thread
         eventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: 1)
     }
     
@@ -21,14 +44,26 @@ final class IOFlowLibNIO : IOFlowLib {
 
     func handleTCPFlowIO(_ flow: NEAppProxyTCPFlow) {
         let (endpointAddress, endpointPort) = getAddressAndPort(endpoint: flow.remoteEndpoint as! NWHostEndpoint)
-        myFlow = flow
-        createNewTCPChannel(host: endpointAddress!, port: endpointPort!)
+        // Setting up handlers for read and write events on the local socket
+        let channel = initTCPChannel(host: endpointAddress!, port: endpointPort!)
+        log(.info, "\(flow.metaData.sourceAppSigningIdentifier) A new TCP socket has been created, bound and connected")
+        // Linking this flow and channel, so that it is always possible to retrive one from the other
+        linkFlowAndChannel(flow, channel)
+        
+        // Scheduling the first read on the flow.
+        // Following ones will be called in the socket write handler
+        scheduleTCPFlowRead(flow)
+        // we are "done", so this function can return.
+        // From this point on:
+        // - any new flow outbound traffic will trigger the flow read completion handler
+        // - any new socket inboud traffic will trigger InboudTCPHandler.channelRead()
     }
 
     func handleUDPFlowIO(_ flow: NEAppProxyUDPFlow) {
     }
     
-    func createNewTCPChannel(host: String, port: Int) {
+    // this function creates, bind and connect a new TCP channel
+    private func initTCPChannel(host: String, port: Int) -> Channel {
         // Bootstrap is a easier way to create a Channel (SocketChannel in our use case)
         var bootstrap = ClientBootstrap(group: eventLoopGroup)
             // Enable SO_REUSEADDR.
@@ -38,84 +73,81 @@ final class IOFlowLibNIO : IOFlowLib {
                 // it may be called multiple times (for example if the hostname
                 // resolves to both IPv4 and IPv6 addresses, cf. Happy Eyeballs).
                 //
-                // add the handlers to the channel pipeline
-                channel.pipeline.addHandlers([
-                    InboudTCPHandler(),
-                    OutboundTCPHandler(),
-                ])
+                // We add only the inbound handler to the channel pipeline.
+                // We don't add a OutboundTCPHandler because SwiftNIO doesn't use it
+                // when using channel.writeAndFlush() ¯\_(ツ)_/¯
+                channel.pipeline.addHandler(InboudTCPHandler())
             }
         bootstrap = try! bootstrap.bind(to: SocketAddress(ipAddress: interfaceAddress, port: 0))
-        try! bootstrap.connect(host: host, port: port).wait()
-        log(.info, "A new TCP socket has been created, bound and connected")
+        return try! bootstrap.connect(host: host, port: port).wait()
+    }
+    
+    private func scheduleTCPFlowRead(_ flow: NEAppProxyTCPFlow) {
+        flow.readData { outboundData, flowError in
+            // when new data is available to read from a flow,
+            // and no errors occurred
+            // we want to write that data to the corresponding socket
+            if flowError == nil, let data = outboundData, !data.isEmpty {
+                log(.debug, "\(flow.metaData.sourceAppSigningIdentifier) TCP flow.readData() has read: \(data)")
+                
+                let channel = getChannel(flow: flow)
+                let buffer = channel.allocator.buffer(bytes: data)
+                getChannel(flow: flow).writeAndFlush(buffer).whenComplete { result in
+                    switch result {
+                    case .success:
+                        log(.debug, "\(flow.metaData.sourceAppSigningIdentifier) TCP data sent successfully through the socket")
+                        // since everything worked as expected, we schedule another read on the flow
+                        self.scheduleTCPFlowRead(flow)
+                    case .failure(let error):
+                        log(.error, "\(flow.metaData.sourceAppSigningIdentifier) \(error) while sending TCP data through the socket")
+                        // TODO: Close flow and channel if an error occurred during socket write
+                    }
+                }
+            } else {
+                log(.error, "\(flow.metaData.sourceAppSigningIdentifier) \((flowError?.localizedDescription) ?? "Empty buffer") occurred during TCP flow.readData()")
+                // TODO: Close flow and channel if an error occurred during flow read
+            }
+        }
+        log(.debug, "\(flow.metaData.sourceAppSigningIdentifier) A new TCP flow readData() has been scheduled")
     }
 }
 
+// in this class we handle receiving data on the socket and
+// writing that data to the flow
 final class InboundTCPHandler: ChannelInboundHandler {
-
     typealias InboundIn = ByteBuffer
     typealias OutboundOut = ByteBuffer
 
-    func channelRead(
-        context: ChannelHandlerContext,
-        data: NIOAny
-    ) {
+    func channelRead(context: ChannelHandlerContext, data: NIOAny) {
         let input = self.unwrapInboundIn(data)
-        guard
-            let bytes = input.getBytes(at: 0, length: input.readableBytes)
-        else {
+        guard let bytes = input.getBytes(at: 0, length: input.readableBytes) else {
             return
         }
-        log(.debug, "there is new inbound TCP socket traffic")
-        // write inbound data to the flow
-        myFlow!.write(Data(bytes)) { flowError in
+        let flow = getFlow(channel: context.channel) as! NEAppProxyTCPFlow
+        log(.debug, "\(flow.metaData.sourceAppSigningIdentifier) There is new inbound TCP socket data")
+        
+        // new traffic is ready to be read on the socket
+        // we want to write that data to the flow
+        flow.write(Data(bytes)) { flowError in
             if flowError == nil {
-                log(.debug, "wrote TCP data successfully to the flow")
+                log(.debug, "\(flow.metaData.sourceAppSigningIdentifier) TCP data has been successfully written to the flow")
+                // no op
+                // the next time data is available to read on the socket
+                // this function will be called again automatically by the event loop
             } else {
-                log(.error, "failed to write TCP data to the flow")
+                log(.error, "\(flow.metaData.sourceAppSigningIdentifier) \(flowError!.localizedDescription) occurred when writing TCP data to the flow")
+                // TODO: Close flow and channel if an error occurred during flow write
             }
         }
     }
 
-    func channelReadComplete(
-        context: ChannelHandlerContext
-    ) {
+    func channelReadComplete(context: ChannelHandlerContext) {
         context.flush()
     }
 
-    func errorCaught(
-        context: ChannelHandlerContext,
-        error: Error
-    ) {
-        print(error)
+    func errorCaught(context: ChannelHandlerContext, error: Error) {
+        log(.error, "\(error) in InboundTCPHandler")
         context.close(promise: nil)
-    }
-}
-
-final class OutboundTCPHandler: ChannelOutboundHandler {
-    typealias OutboundIn = ByteBuffer
-    typealias InboundOut = ByteBuffer
-
-    func channelWrite(
-        context: ChannelHandlerContext,
-        data: NIOAny
-    ) {
-        log(.debug, "there is new outbound TCP socket traffic")
-        // do something with outbound data
-    }
-
-
-    func channelWriteComplete(
-        context: ChannelHandlerContext
-    ) {
-        context.flush()
-    }
-
-    func errorCaught(
-        context: ChannelHandlerContext,
-        error: Error
-    ) {
-        print(error)
-
-        context.close(promise: nil)
+        // TODO: Close flow and channel if an error occurred during socket read
     }
 }
