ce525a5920066d3cf5ebe3590f9774a899c39c87
kp-miguel-galindo <miguel.galindo@kape.com>
2024-01-31 15:38:06 +0100

    PIA-1242: Hide most output from test

--- build_scripts/test.sh
+++ build_scripts/test.sh
@@ -8,11 +8,15 @@ then
     source .env
 fi
 
+test_output_file=$(mktemp)
 xcodebuild -project ${PROJECT}.xcodeproj \
-           -scheme ${TEST_TARGET} test
+           -scheme ${TEST_TARGET} test 2>&1 | tee "$test_output_file" | grep 'Test '
 
 if [ "$?" -ne 0 ]
 then
     echo "Testing failed with error $1"
+    echo "::group::Full output"
+    cat "$test_output_file"
+    echo "::endgroup::"
     exit $1
 fi
