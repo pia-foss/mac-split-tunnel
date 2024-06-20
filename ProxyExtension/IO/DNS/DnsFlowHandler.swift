c427fafb109c6745be4b57a0a3c1af279b34c7ce
Michele Emiliani <michele.emiliani@kape.com>
2024-06-20 18:23:59 +0200

    PIA-1940: Implement follow app rules logic
    Using mock DNS servers for now

--- ProxyExtension/IO/DNS/DnsFlowHandler.swift
+++ ProxyExtension/IO/DNS/DnsFlowHandler.swift
@@ -26,19 +26,57 @@ final class DnsFlowHandler: FlowHandlerProtocol {
         // We need to handle two modes here:
         // - Follow App Rules
         // - VPN DNS Only
-        // assume for now that Name Servers is set to Follow App Rules
-        // if(name server is follow app rules)
-        //     if(app is bypass app)
-        //         proxy to physical
-        //     else
-        //         if(vpn is disconnected)
-        //             block
-        //         else
-        //             proxy to vpn //using current DNS settings
-        return startProxySession(flow: flow, vpnState: vpnState)
+        if(vpnState.dnsFollowAppRules) {
+            // TODO: Fetch these from vpnState. PIA-1942
+            // These are the backed up original ISP DNS servers.
+            let ispDnsServers: [String] = ["1.1.1.1"]
+            // This will probably be just the PIA server we are connected to
+            let piaDnsServers: [String] = ["8.8.8.8"]
+            switch dnsPolicyFor(flow: flow, vpnState: vpnState) {
+            case .proxyToPhysical:
+                return startProxySession(flow: flow, vpnState: vpnState, dnsServers: ispDnsServers)
+            case .proxyToVpn:
+                return startProxySession(flow: flow, vpnState: vpnState, dnsServers: piaDnsServers)
+            case .block:
+                return false
+            }
+        } else {
+            // currently not handling any DNS requests if mode is VPN DNS Only
+            return false
+        }
     }
 
-    private func startProxySession(flow: Flow, vpnState: VpnState) -> Bool {
+    // The DNS policy we should apply to an app
+    enum DnsPolicy {
+        case proxyToPhysical, proxyToVpn, block
+    }
+
+    // The policy logic is easier compared to the transparent proxy one.
+    // bypass mode: always proxy to physical.
+    // only vpn mode: always proxy to vpn. Block if the vpn is disconnected
+    // unspecified mode: always proxy to the default route interface
+    private func dnsPolicyFor(flow: Flow, vpnState: VpnState) -> DnsPolicy {
+        let mode = FlowPolicy.modeFor(flow: flow, vpnState: vpnState)
+        if(mode == AppPolicy.Mode.bypass) {
+            return .proxyToPhysical
+        } else if(mode == AppPolicy.Mode.vpnOnly) {
+            if(vpnState.isConnected) {
+                return .proxyToVpn
+            } else {
+                return .block
+            }
+        } else { // unspecified case, it means the app has no specific settings
+            if(vpnState.routeVpn) { // normal ST mode, vpn has the default route
+                return .proxyToVpn
+            } else {
+                return .proxyToPhysical
+            }
+        }
+    }
+
+    // Instead of using the original endpoint as in the transparent proxy,
+    // we force a specific server for the DNS request, based on
+    private func startProxySession(flow: Flow, vpnState: VpnState, dnsServers: [String]) -> Bool {
         let interface = networkInterfaceFactory.create(interfaceName: vpnState.bindInterface)
 
         // Verify we have a valid bindIp - if not, trace it and ignore the flow
